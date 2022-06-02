d63bc5957533e78d0504c6299f43ed7202fdddaf
atienoobwanda <atienoobwanda@gmail.com>
2022-06-02 20:13:36 +0300

    updating views

--- blogApp/views.py
+++ blogApp/views.py
@@ -1,30 +1,32 @@
 from django.shortcuts import render
+from .models import *
 
-posts = [
-    {
-        'author': 'Atieno',
-        'content': 'First dummy post',
-        'date_posted': 'May 27 2018'
-    },
-     {
-        'author': 'Kikie',
-        'content': 'Second dummy post',
-        'date_posted': 'June 27 2019'
-    },
-     {
-        'author': 'Treasure',
-        'content': 'Third dummy post',
-        'date_posted': 'May 27 2020'
-    },
-     {
-        'author': 'Atieno',
-        'content': 'Fourth dummy post',
-        'date_posted': 'May 27 2021'
-    }
-]
+# posts = [
+#     {
+#         'author': 'Atieno',
+#         'content': 'First dummy post',
+#         'date_posted': 'May 27 2018'
+#     },
+#      {
+#         'author': 'Kikie',
+#         'content': 'Second dummy post',
+#         'date_posted': 'June 27 2019'
+#     },
+#      {
+#         'author': 'Treasure',
+#         'content': 'Third dummy post',
+#         'date_posted': 'May 27 2020'
+#     },
+#      {
+#         'author': 'Atieno',
+#         'content': 'Fourth dummy post',
+#         'date_posted': 'May 27 2021'
+#     }
+# ]
 
 # Create your views here.
 def home(request):
+    posts = posts.objects.all()
     return render(request, 'blog/index.html', {"posts": posts})
 
 def about(request):
