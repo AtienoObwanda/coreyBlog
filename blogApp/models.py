a01d1a15cbe24e2bdb8b02ec5c23191785cfcd4a
atienoobwanda <atienoobwanda@gmail.com>
2022-06-02 20:12:03 +0300

    Adding models and migrations

--- blogApp/models.py
+++ blogApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Models
+class posts(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now) # keeps added date, default is used when you wat to be able to use lastEdited date 
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # If a user is deleted, their posts get deleted. 
