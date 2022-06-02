53038e6803b935600a37f06441576a2e7cb56009
atienoobwanda <atienoobwanda@gmail.com>
2022-06-02 19:45:08 +0300

    Adding dummy posts

--- blogApp/views.py
+++ blogApp/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Atieno',
+        'content': 'First dummy post',
+        'date_posted': 'May 27 2018'
+    },
+     {
+        'author': 'Kikie',
+        'content': 'Second dummy post',
+        'date_posted': 'June 27 2019'
+    },
+     {
+        'author': 'Treasure',
+        'content': 'Third dummy post',
+        'date_posted': 'May 27 2020'
+    },
+     {
+        'author': 'Atieno',
+        'content': 'Fourth dummy post',
+        'date_posted': 'May 27 2021'
+    }
+]
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/index.html')
+    return render(request, 'blog/index.html', {"posts": posts})
 
 def about(request):
     return render(request, 'blog/about.html')
