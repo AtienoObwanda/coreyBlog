38654ed896b03c83dadc240b0393901cf890b5e3
atienoobwanda <atienoobwanda@gmail.com>
2022-06-02 19:34:19 +0300

    updating nav bar urls, adding a view for about page

--- blogApp/views.py
+++ blogApp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'blog/index.html')
+
+def about(request):
+    return render(request, 'blog/about.html')
