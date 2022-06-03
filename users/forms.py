c9481a3c8fd7726b57f750c8c6ee04cbfe2e99f5
atienoobwanda <atienoobwanda@gmail.com>
2022-06-03 09:00:20 +0300

    adding crispy forms

--- users/forms.py
+++ users/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
-class UserREgistrationForm(UserCreationForm):
+class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
--- users/views.py
+++ users/views.py
@@ -1,20 +1,22 @@
 from urllib import request
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.http  import HttpResponse, Http404, HttpResponseRedirect
+
+from .forms import *
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created successfully for {username}!')
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = UserRegistrationForm()
     return render(request, 'users/register.html', {'form': form})
 
 #
