7e130c5fdc22f826d29f65a013f14d67f9507a8b
atienoobwanda <atienoobwanda@gmail.com>
2022-07-22 11:39:20 +0300

    C9

--- Ammends/JULY/jul22.py
+++ Ammends/JULY/jul22.py
@@ -222,3 +222,52 @@ def register(request):
 def profile(request):
 
     return render(request, 'users/profile.html')
+from urllib import request
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.http  import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .forms import *
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created successfully! You can now login')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+
+    return render(request, 'users/profile.html')from urllib import request
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.http  import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .forms import *
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created successfully! You can now login')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+
+    return render(request, 'users/profile.html')
