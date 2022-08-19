ddd45b735a59d2f63bc44d572d12bfeba4b326a9
atienoobwanda <atienoobwanda@gmail.com>
2022-08-19 13:38:36 +0300

    C17

--- app/Ammends/AUG/a19.py
+++ app/Ammends/AUG/a19.py
@@ -3980,3 +3980,252 @@ def register(request):
 @login_required
 def profile(request):
 
+from urllib import request
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.http  import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .forms import *
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created successfully! You can now login')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+
+    return render(request, 'users/profile.html')
+from urllib import request
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.http  import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .forms import *
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created successfully! You can now login')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+
+    return render(request, 'users/profile.html')
+from urllib import request
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.http  import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .forms import *
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created successfully! You can now login')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+
+    return render(request, 'users/profile.html')
+from urllib import request
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.http  import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .forms import *
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created successfully! You can now login')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+
+    return render(request, 'users/profile.html')from urllib import request
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.http  import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .forms import *
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created successfully! You can now login')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+
+    return render(request, 'users/profile.html')
+from urllib import request
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.http  import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .forms import *
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created successfully! You can now login')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+
+    return render(request, 'users/profile.html')
+from urllib import request
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.http  import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .forms import *
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created successfully! You can now login')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+
+    return render(request, 'users/profile.html')
+
+from urllib import request
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.http  import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .forms import *
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created successfully! You can now login')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+
+    return render(request, 'users/profile.html')
+from urllib import request
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.http  import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .forms import *
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created successfully! You can now login')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+
+    return render(request, 'users/profile.html')
+from urllib import request
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.http  import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .forms import *
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created successfully! You can now login')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+
